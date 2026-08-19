1b793fca9c9bf846bb26a6e2a89fe8bfb6fa2f9b
UrunoRikuto <182198322+UrunoRikuto@users.noreply.github.com>
2026-08-19 16:25:11 +0900

    feat: 射程を実装

--- Assets/Scripts/Weapon/CS_PistolWeapon.cs
+++ Assets/Scripts/Weapon/CS_PistolWeapon.cs
@@ -38,6 +38,13 @@ public class CS_PistolWeapon : CS_BaseWeapon
             // “G‚ªŒ©‚Â‚©‚ç‚È‚¢ê‡‚ÍA’Êí‚Ì”­Ë•ûŒü‚Å’e‚ğ”­Ë
             bullet.Activate(firePoint.position, firePoint.rotation);
         }
+
+        // ’e‚Ìí—Ş‚É‰‚¶‚½İ’è
+        if (bullet is CS_SimpleBullet)
+        {
+            CS_SimpleBullet simpleBullet = bullet as CS_SimpleBullet;
+            simpleBullet.SetRange(weaponData.range);
+        }
     }
 
     //Æ€•ûŒü‚Ìˆê”Ô‹ß‚¢“G‚ğ‘_‚¤ˆ—
