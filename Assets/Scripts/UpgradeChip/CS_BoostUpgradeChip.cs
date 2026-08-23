b1e6a4accd6cbe950b9bf80637b85ca29592b413
宇留野陸斗 <shiguma665hal@gmail.com>
2026-08-24 02:50:12 +0900

    feat: ステータス変化系のチップ効果の作成 (#98)
    Co-authored-by: UrunoRikuto <182198322+UrunoRikuto@users.noreply.github.com>

---
+++ Assets/Scripts/UpgradeChip/CS_BoostUpgradeChip.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+
+public class CS_BoostUpgradeChip : CS_UpgradeChipBase
+{
+    [SerializeField]
+    [Header("ブーストエネルギー増加量")]
+    private int _boostEnergyIncreaseAmount = 100;
+
+    [SerializeField]
+    [Header("ブースト消費軽減率")]
+    private float _boostConsumptionReductionRate = 0.1f;
+
+    [SerializeField]
+    [Tooltip("ブースト時の移動速度増加率")]
+    private float _boostSpeedIncreaseRate = 0.5f;
+
+
+    protected override void ApplyEffectLevel1()
+    {
+        // ブーストエネルギー増加量を100増加
+        _chipManager.upgradeStatus.boostEnergyIncreaseAmount += _boostEnergyIncreaseAmount;
+    }
+
+    protected override void ApplyEffectLevel2()
+    {
+        // ブーストエネルギー自然回復エフェクトコンポーネントを追加させる
+        // 対象：プレイヤー
+    }
+
+    protected override void ApplyEffectLevel3()
+    {
+        // ブースト消費軽減率を10%加算
+        _chipManager.upgradeStatus.boostConsumptionReductionRate += _boostConsumptionReductionRate;
+    }
+
+    protected override void ApplyEffectLevel4()
+    {
+        // ブースト時の移動速度増加率を50%加算
+        _chipManager.upgradeStatus.boostSpeedIncreaseRate += _boostSpeedIncreaseRate;
+    }
+
+    protected override void ApplyEffectLevel5()
+    {
+        // 敵を倒したときにブーストエネルギーを5回復するエフェクトを追加させる
+        // 対象：プレイヤー
+    }
+}
---
+++ Assets/Scripts/UpgradeChip/CS_WeaponStatusUpgradeChip.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+
+public class CS_WeaponStatusUpgradeChip : CS_UpgradeChipBase
+{
+    [SerializeField]
+    [Header("武器の弾数増加率")]
+    private float _bulletCountIncreaseRate = 0.5f;
+
+    [SerializeField]
+    [Header("武器の連射速度増加率")]
+    private float _fireRateIncreaseRate = 0.3f;
+
+    [SerializeField]
+    [Header("武器のリロード時間減少率")]
+    private float _reloadTimeReductionRate = 0.3f;
+
+
+    protected override void ApplyEffectLevel1()
+    {
+        // 武器の弾数増加率を50%加算
+        _chipManager.upgradeStatus.bulletCountIncreaseRate += _bulletCountIncreaseRate;
+    }
+
+    protected override void ApplyEffectLevel2()
+    {
+        // 武器の連射速度増加率を30%加算
+        _chipManager.upgradeStatus.fireRateIncreaseRate += _fireRateIncreaseRate;
+    }
+
+    protected override void ApplyEffectLevel3()
+    {
+        // 武器のリロード時間減少率を30%加算
+        _chipManager.upgradeStatus.reloadSpeedIncreaseRate += _reloadTimeReductionRate;
+    }
+
+    protected override void ApplyEffectLevel4()
+    {
+        // HPが少ない敵を優先的に狙うようになるエフェクトを追加させる
+        // 対象: プレイヤーの武器
+    }
+
+    protected override void ApplyEffectLevel5()
+    {
+        // 発射される弾の数が+1されるエフェクトを追加させる
+        // 対象: プレイヤーの武器
+        // (ショットガンの場合は5ペレットの場合、10ペレットになる)
+    }
+}
