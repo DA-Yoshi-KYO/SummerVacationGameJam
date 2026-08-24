d6032919c966c57824c9902fe876527732581441
UrunoRikuto <182198322+UrunoRikuto@users.noreply.github.com>
2026-08-24 20:21:10 +0900

    feat: 与えたダメージの一定割合の数値分回復するエフェクトの処理部分作成

---
+++ Assets/Scripts/UpgradeChip/Effect/CS_LifeStealEffect.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class CS_LifeStealEffect : MonoBehaviour
+{
+    [Tooltip("回復する割合")]
+    private float _healRate = 0.1f;
+
+    [Tooltip("プレイヤーのステータス参照")]
+    private CS_PlayerStatus _playerStatus;
+
+    public void ApplyEffect(float damage)
+    {
+        float healAmount = damage * _healRate;
+        _playerStatus.Regenerate(healAmount);
+    }
+
+    public void SetHealRate(float healRate)
+    {
+        _healRate = healRate;
+    }
+
+    public void SetPlayerStatus(CS_PlayerStatus playerStatus)
+    {
+        _playerStatus = playerStatus;
+    }
+}
