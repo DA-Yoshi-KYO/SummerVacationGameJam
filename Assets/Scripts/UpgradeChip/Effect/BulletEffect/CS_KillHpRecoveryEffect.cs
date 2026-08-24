5b91d98a5d87529a21bda8dedb013b23d226a0a0
UrunoRikuto <182198322+UrunoRikuto@users.noreply.github.com>
2026-08-24 20:03:52 +0900

    feat: 敵を倒したときにHP回復するエフェクトの処理部分作成

---
+++ Assets/Scripts/UpgradeChip/Effect/BulletEffect/CS_KillHpRecoveryEffect.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class CS_KillHpRecoveryEffect : MonoBehaviour
+{
+    [SerializeField]
+    [Tooltip("敵を倒した時に回復するHPの量")]
+    private float _recoveryHpAmount = 5f;
+
+    [SerializeField]
+    [Tooltip("プレイヤーのステータス参照")]
+    private CS_PlayerStatus _playerStatus;
+
+    public void ApplyEffect()
+    {
+        // プレイヤーの体力を回復する
+        _playerStatus.Regenerate(_recoveryHpAmount);
+    }
+
+    public void SetRecoveryHpAmount(float amount)
+    {
+        _recoveryHpAmount = amount;
+    }
+
+    public void SetPlayerStatus(CS_PlayerStatus playerStatus)
+    {
+        _playerStatus = playerStatus;
+    }
+}
